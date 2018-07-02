5936d4b6f5efe7d3f5a545cca446caa45b5688ca
Inspirotizzy <40707638+Inspirotizzy@users.noreply.github.com>
2018-07-02 22:19:10 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,459 @@
+<?php 
+$server='localhost'; $username='root'; $password=''; $dbname='LEARN';
+$connect=mysqli_connect($server,$username,$password,$dbname);
+
+
+?>
+	 <!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>O'Bounce Technologies</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+<script>
+alert ("Welcome to O'bounce Techonologies Website");
+</script>
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="upper-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-4">
+							<p>Welcome to O'Bounce Technology Institute</p>
+						</div>
+						<div class="col-xs-6 col-md-push-2 text-right">
+							<p>
+								<ul class="colorlib-social-icons">
+									<li><a href="https://twitter.com/obouncetech"><i class="icon-twitter"></i></a></li>
+									<li><a href="https://www.facebook.com/obounce"><i class="icon-facebook"></i></a></li>
+									<li><a href="https://www.linkedin.com/company/o"><i class="icon-linkedin"></i></a></li>
+									
+								</ul>
+							</p>
+							<p class="btn-apply"><a href="#">Apply Now</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2">
+							<div id="colorlib-logo"><a href="index.php"><img src="obounceLogo-300x76.png"></a>
+                            </div>
+						</div>
+						<div class="col-md-10 text-right menu-1">
+							<ul>
+								<li class="active"><a href="index.php">Home</a></li>
+								<li class="has-dropdown">
+									<a href="courses.html">Courses</a>
+									<ul class="dropdown">
+										<li><a href="courses-single.html">Courses Single</a></li>
+										<li><a href="#">Mobile Apps</a></li>
+										<li><a href="#">Website</a></li>
+										<li><a href="#">Web Design</a></li>
+										<li><a href="#">WordPress</a></li>
+									</ul>
+								</li>
+								<li><a href="about.php">About</a></li>
+								
+								
+								<li><a href="contact.php">Contact</a></li>
+								<li class="btn-cta"><a href="#"><span>Free Trial</span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		
+		
+		
+		<aside id="colorlib-hero">
+			<div class="flexslider">
+				<ul class="slides">
+			   	<li style="background-image: url(images/img_bg_1.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-8 col-sm-12 col-md-offset-2 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>O'Bounce Technologies Institute</h1>
+				   					<p><a href="reg.php" class="btn btn-primary btn-lg btn-learn">Register Now</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_2.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-8 col-sm-12 col-md-offset-2 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>The Conveyors of Dreamed  Innovations </h1>
+				   					<p><a href="#" class="btn btn-primary btn-lg btn-learn">Free Trial</a></p>
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_3.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-8 col-sm-12 col-md-offset-2 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>The Best Learning Center</h1>
+				   					<p><a href="reg.php" class="btn btn-primary btn-lg btn-learn">Register Now</a></p>
+				   				</div>
+				   			</div>
+				   		</div>   
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/img_bg_4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-8 col-sm-12 col-md-offset-2 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<h1>Knowledge is the Key to Success</h1>
+				   				
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>	
+			  	</ul>
+		  	</div>
+			</div>
+			</div>
+			</div>
+			</div>
+			</div>
+			</div>
+		</aside>
+		
+		
+					
+			<div id="colorlib-intro">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 intro-wrap">
+						<div class="intro-flex">
+							<div class="one-third color-1 animate-box">
+								<span class="icon"><i class="flaticon-market"></i></span>
+								<div class="desc">
+									<h3>Learn Courses Online</h3>
+									<p><a href="#" class="view-more">View More</a></p>
+								</div>
+							</div>
+							<div class="one-third color-2 animate-box">
+								<span class="icon"><i class="flaticon-open-book"></i></span>
+								<div class="desc">
+									<h3>Online Library Store</h3>
+									<p><a href="#" class="view-more">View More</a></p>
+								</div>
+							</div>
+							<div class="one-third color-3 animate-box">
+								<div class="desc2">
+									<h3>50% off in all selected Courses</h3>
+									<p><a href="#" class="view-more">View More</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					<div class="col-md-8">
+						<div class="about-desc animate-box">
+							<h2>Welcome to O'Bounce Technologies Learning Institute</h2>
+							<p>This is a Great Citadel of Learning where your ambition of becoming a great programmer comes to true.You cannot not be in  a better place than where you are!It a place you make a full discovery of yourself<p>
+						<div class="fancy-collapse-panel">
+	                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+	                     <div class="panel panel-default">
+	                         <div class="panel-heading" role="tab" id="headingOne">
+	                             <h4 class="panel-title">
+	                                 <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Why choose us?
+	                                 </a>
+	                             </h4>
+	                         </div>
+	                         <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+	                             <div class="panel-body">
+	                                 <div class="row">
+									      		<div class="col-md-6">
+												<p> We give our student the best trainig in other to solve problem because the company stands to solve problem: We don't only MAKE but we also SOLVE!!!!
+									      		</div>
+									      		<div class="col-md-6">
+													<p> We provide a Learning Network that Inspire Innovations
+									      		</div>
+									      	</div>
+	                             </div>
+	                         </div>
+	                     </div>
+	                     <div class="panel panel-default">
+	                         <div class="panel-heading" role="tab" id="headingTwo">
+	                             <h4 class="panel-title">
+	                                 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Courses
+	                                 </a>
+	                             </h4>
+	                         </div>
+	                         
+									<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+	                             <div class="panel-body">
+	                                 
+									 		<ul>
+													<li>Front-End Web Developer</li>
+													<li>Back-End Web Developer</li>
+													<li>Data-Analyst</li>
+													<li>Artificial Intelligent Study</li>
+												</ul>
+	                             </div>
+	                         </div>
+	                     </div>
+	                     	<div class="panel panel-default">
+	                         <div class="panel-heading" role="tab" id="headingTwo">
+	                             <h4 class="panel-title">
+	                                 <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Courses
+	                                 </a>
+	                             </h4>
+	                         </div>
+							 
+							 
+							 
+							<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+	                             <div class="panel-body">
+	                                 
+									 		<ul>
+													<li>Software Developement</li>
+													<li>Digital Marketing</li>
+													<li>Mobile App Development</li>
+													<li>Web Development</li>
+												</ul>
+	                             </div>
+	                         </div>
+	                     </div>
+	                     
+						 
+						 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>		
+			
+	 
+	 
+	 
+	 
+	 
+	 <div id="colorlib-services">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-books"></i>
+							</span>
+							<div class="desc">
+								<h3>Professional Courses</h3>
+								<p>A lot of Web Professional Courses Available</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-professor"></i>
+							</span>
+							<div class="desc">
+								<h3>Experienced Instructor</h3>
+								<p>The Best of Tutors and Inventors you never imagined</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-book"></i>
+							</span>
+							<div class="desc">
+								<h3>Practical Training</h3>
+								<p>The Epic of the realistic of the Training is second to none.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="flaticon-diploma"></i>
+							</span>
+							<div class="desc">
+								<h3>Validated Certificate</h3>
+								<p>Renowned and Recognised Certificate: certified by Google.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+ 
+
+ 
+<hr>
+
+
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 <table style="width:100%">
+	 <tr>
+	 <th style="text-align:left; font-family:Comic Sans MS;">A HOME OF DISCOVERY OF TALENT</th>
+	 
+	 <br><br><br>
+	 <th style="text-align:left;font-family:Comic Sans MS;">JOINT LEARNING MADE EASY</th>
+	 
+	 <th style="text-align:left;font-family:Comic Sans MS;">CONSISTENCY IS THE KEY</th>
+	 </tr>
+	 <tr>
+	 <td><img src="code men.jpg"></td>
+	 <td><img src="group code.jpg"></td>
+	 <td><img src="real group.jpg"></td>
+	 </tr>
+	 </table>
+	
+ <br><br><br><br><br><br><br><br>
+<div style="float: right;"><img src="bb logo.png">
+</div>
+
+<em style="color:blue;">O'Bouce Technologies Learning Institute<strong style="color:red;"> (Conveyors of Dreamed Innovation)</strong> is Place where innovations can be made possible: The Dream of your's will be made possible..</em>
+	 
+	 <!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	 
+  
+		 
+	 
+	 </html>
